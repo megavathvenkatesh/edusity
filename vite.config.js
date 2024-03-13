731a1cf5e6c50a4971545afa4ddece012487ad1a
megavth venkatesh <144617554+megavathvenkatesh@users.noreply.github.com>
2024-03-13 22:16:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      // Add Rollup options here
+      rollupOptions: {
+        // Specify external modules to exclude from bundling
+        external: ['/src/main.jsx'],
+      },
+    }),
+  ],
+});
